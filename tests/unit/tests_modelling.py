53c2244f05136f8057076e5cdcba7f0455512913
Llorenç Buil <llorensbf@gmail.com>
2024-06-12 14:35:15 +0000

    start testing

---
+++ tests/unit/tests_modelling.py
@@ -0,0 +1,13 @@
+from stock_portfolio_tracker.modelling._utils import calculate_current_percent_gain
+import pytest
+import pandas as pd
+import numpy as np
+
+
+@pytest.fixture
+def transactions():
+    pd.DataFrame({
+        'date': ["2024-01-01", "2024-01-02", "2024-01-03", "2024-01-04", "2024-01-05", "2024-01-06", "2024-01-06", "2024-01-07"],
+        'value': [-1000, np.nan, 120, np.nan, np.nan, -600, 130, -100],
+        'current_portfolio_value': [0, 1100, 1200, 1080, 1300, 1500, 1000]
+    })
